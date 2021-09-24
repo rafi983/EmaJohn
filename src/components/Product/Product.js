ec8195c3f472fd65c349b6f0e5ea3d0e16606de5
rafi983 <rafiirfan211@gmail.com>
2021-09-24 18:34:29 +0600

    star rating done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import Rating from "react-rating";
 
 const Product = (props) => {
-  const { img, name, seller, price, stock, features } = props.product;
+  const { img, name, seller, price, stock, features, star } = props.product;
+  const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
   return (
     <div className="product">
       <div className="product-img">
@@ -10,7 +14,7 @@ const Product = (props) => {
       </div>
       <div className="product-info">
         <div>
-          <h5>{name}</h5>
+          <h5 className="pd-name">{name}</h5>
           <p>by {seller}</p>
           <h2>{price}$</h2>
           <p>only {stock} left in stock - order soon</p>
@@ -18,15 +22,24 @@ const Product = (props) => {
             className="buy-btn"
             onClick={() => props.handleAddToCart(props.product)}
           >
-            add to cart
+            {cartIcon} add to cart
           </button>
         </div>
 
-        <div className="product-feature">
-          <h3>Features</h3>
-          {features.map((feature) => (
-            <Feature feature={feature}></Feature>
-          ))}
+        <div>
+          <div className="product-feature">
+            <Rating
+              className="rating"
+              initialRating={star}
+              readonly
+              emptySymbol="far fa-star"
+              fullSymbol="fas fa-star"
+            />
+            <h3>Features</h3>
+            {features.map((feature) => (
+              <Feature feature={feature}></Feature>
+            ))}
+          </div>
         </div>
       </div>
     </div>
